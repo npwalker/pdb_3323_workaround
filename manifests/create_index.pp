2c2b2a64d7ea01a1e41c0c3b45097e1ca51d5a5d
Nick Walker <nick.walker@puppetlabs.com>
2017-03-08 10:05:57 -0800

    Namespace postgresql_psql to provide broad compatibility
    the onlyif parameter on postgresql_psql is only available on 4.5+.
    Since this module needs to be compatible with as many users as
    possible having that constraint isn't acceptable.
    
    This commit copies the current postgresql_psql from the master
    branch of postgresql into this module and namespaces it so it
    won't conflict with user's own version of postgresql.
    
    As a result this module ends up requiring no dependencies.

--- manifests/create_index.pp
+++ manifests/create_index.pp
@@ -4,7 +4,7 @@ define pdb_3323_workaround::create_index (
   String $table_name,
   String $column_name,
 ) {
-  postgresql_psql { "Create index ${index_name}" :
+  pdb_3323_workaround_postgresql_psql { "Create index ${index_name}" :
       command    => "CREATE INDEX ${index_name} ON ${table_name}(${column_name})",
       unless     => "SELECT 1 FROM pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace WHERE c.relname = '${index_name}'",
       db         => $db,
--- manifests/drop_index.pp
+++ manifests/drop_index.pp
@@ -4,7 +4,7 @@ define pdb_3323_workaround::drop_index (
   String $table_name,
   String $column_name,
 ) {
-  postgresql_psql { "Drop index ${index_name}" :
+  pdb_3323_workaround_postgresql_psql { "Drop index ${index_name}" :
       command    => "DROP INDEX ${index_name}",
       onlyif     => "SELECT 1 FROM pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace WHERE c.relname = '${index_name}'",
       db         => $db,
